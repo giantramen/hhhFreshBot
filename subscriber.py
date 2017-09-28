a3bae50d2c79a0c951003f0f8c2ba361cc6c8075
Will Crawford <will@ccrawford.org>
2017-09-28 03:02:40 -0500

    Add files via upload

---
+++ subscriber.py
@@ -0,0 +1,7 @@
+class Subscriber:
+    def __init__(self):
+        self.phoneNumber = 0
+        self.carrier = ""
+        self.emailAddress = ""
+        self.upvoteThreshold = 0
+        self.seenPostIds = []
